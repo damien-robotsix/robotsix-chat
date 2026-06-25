d96c3e14a44ec4b37cf48b6666b1c47b706c42b6
robotsix-mill <mill@robotsix.local>
2026-06-25 13:10:21 +0000

    mill(docs): Close a session and clean up its associated background tasks (20260625T122325Z-close-a-session-and-clean-up-its-associa-3f18)

---
+++ _stubs/asgi_correlation_id/__init__.py
@@ -0,0 +1,6 @@
+class CorrelationIdMiddleware:
+    def __init__(self, app, *args, **kwargs):
+        self.app = app
+
+    async def __call__(self, scope, receive, send):
+        await self.app(scope, receive, send)
